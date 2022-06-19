7714a231fbcc77a62c592ab0f8f04ea142266b34
RabinsRai <rai.rabins@gmail.com>
2022-06-19 10:29:03 +0530

    Adding New HelloWorld With Validation tutorial

---
+++ HelloWorld2/HelloWorld/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HelloWorld
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.config");
+            routes.MapRoute(
+               name: "Home",
+               url: "Home",
+               defaults: new { controller = "Home", action = "GotoHome", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+              name: "Home1",
+              url: "Home/Home",
+              defaults: new { controller = "Home", action = "GotoHome", id = UrlParameter.Optional }
+          );
+           
+            routes.MapRoute(
+               name: "Default",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+           );
+        }
+    }
+}
